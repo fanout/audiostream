78e30a48a62dbb128806b22e74461115157ce75f
Justin Karneges <justin@karneges.com>
2017-02-02 19:43:28 -0800

    improve the streaming

--- audiostream/management/commands/play.py
+++ audiostream/management/commands/play.py
@@ -8,31 +8,56 @@ from django.core.management.base import BaseCommand
 
 zmq_context = zmq.Context()
 
-def _publish_worker():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.bind(('127.0.0.1', 5004))
+def _play(filename):
+	subprocess.check_call(['gst-launch-1.0', 'filesrc', 'location=%s' % filename, '!', 'decodebin', '!', 'queue', '!', 'audioconvert', '!', 'lamemp3enc', '!', 'udpsink', 'clients=localhost:5004'])
+
+def _publish_chunk(sock, data):
+	m = {
+		'channel': 'audio',
+		'formats': {
+			'http-stream': {
+				'content': data
+			}
+		}
+	}
+	sock.send(tnetstring.dumps(m), zmq.DONTWAIT)
+
+def _input_worker():
+	in_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	in_sock.bind(('127.0.0.1', 5004))
+
+	out_sock = zmq_context.socket(zmq.PUSH)
+	out_sock.connect('inproc://input')
+
+	while True:
+		data, addr = in_sock.recvfrom(65536)
+		out_sock.send_multipart(['data', data])
+
+def _publish_worker(cond):
+	cond.acquire()
+
+	in_sock = zmq_context.socket(zmq.PULL)
+	in_sock.bind('inproc://input')
+
+	cond.notify()
+	cond.release()
 
 	out_sock = zmq_context.socket(zmq.PUSH)
 	out_sock.connect('tcp://localhost:5560')
 
 	buf = ''
-	while True:
-		data, addr = sock.recvfrom(65536)
-		buf += data
-		if len(buf) >= 16384:
-			m = {
-				'channel': 'audio',
-				'formats': {
-					'http-stream': {
-						'content': buf
-					}
-				}
-			}
-			out_sock.send(tnetstring.dumps(m), zmq.DONTWAIT)
-			buf = ''
+	packets = 0
 
-def _play(filename):
-	subprocess.check_call(['gst-launch-1.0', 'filesrc', 'location=%s' % filename, '!', 'decodebin', '!', 'queue', '!', 'audioconvert', '!', 'lamemp3enc', '!', 'udpsink', 'clients=localhost:5004'])
+	while True:
+		msg = in_sock.recv_multipart()
+		if msg[0] == 'data':
+			buf += msg[1]
+			packets += 1
+
+		if (msg[0] == 'flush' and packets > 0) or packets >= 40:
+			_publish_chunk(out_sock, buf)
+			buf = ''
+			packets = 0
 
 class Command(BaseCommand):
 	help = 'Background cleanup task'
@@ -41,10 +66,22 @@ class Command(BaseCommand):
 		parser.add_argument('filename')
 
 	def handle(self, *args, **options):
-		thread = threading.Thread(target=_publish_worker)
-		thread.daemon = True
-		thread.start()
+		cond = threading.Condition()
+		cond.acquire()
+		publish_thread = threading.Thread(target=_publish_worker, args=(cond,))
+		publish_thread.daemon = True
+		publish_thread.start()
+		cond.wait()
+		cond.release()
+
+		input_thread = threading.Thread(target=_input_worker)
+		input_thread.daemon = True
+		input_thread.start()
+
+		sock = zmq_context.socket(zmq.PUSH)
+		sock.connect('inproc://input')
 
 		while True:
 			_play(options['filename'])
+			sock.send_multipart(['flush'])
 			time.sleep(5)

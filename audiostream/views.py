caf6d275629c5b394b361932025fe2ab6085c487
Justin Karneges <justin@karneges.com>
2017-02-03 15:43:40 -0800

    send buffer on connect

--- audiostream/views.py
+++ audiostream/views.py
@@ -1,6 +1,10 @@
+import redis
 from django.http import HttpResponse
 from django_grip import set_hold_stream
 
+r = redis.Redis()
+
 def home(request):
+	body = ''.join(reversed(r.lrange('bufs', 0, -1)))
 	set_hold_stream(request, 'audio')
-	return HttpResponse(content_type='audio/mpeg')
+	return HttpResponse(body, content_type='audio/mpeg')
